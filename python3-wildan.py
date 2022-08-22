fa40fa80b170cad9e92147977eacceb83a26b2b7
wildan <111479965+wildanbcil@users.noreply.github.com>
2022-08-21 23:44:58 -0700

    Add files via upload

---
+++ python3-wildan.py
@@ -0,0 +1,15 @@
+nama_depan=input("masukan nama depan: ")
+nama_belakang=input("masukan nama belakang: ")
+print("nama lengkap saya adalah",nama_depan,nama_belakang)
+
+a=int(input("masukan nilai A:"))
+b=float(input("masukan nilai B:"))
+c=a+b
+
+print("A=",a)
+print("B=",b)
+print("c=",c)
+
+x=int(input("masukan suhu celcius"))
+y=(9/5*x)  + 32
+print(x,"celcius=",y, "fahreheit")
