fe7ec7db4905a4258a21adaeeada9246e22453ef
wildan <111479965+wildanbcil@users.noreply.github.com>
2022-08-21 23:44:18 -0700

    Add files via upload

---
+++ python4-wildan.py
@@ -0,0 +1,20 @@
+print("persegi\n")
+
+sisi = int(input("masukan nilai sisi: "))
+luas_p=sisi * sisi
+print("luas persegi adalah",luas_p)
+
+print("2,persegi panjang")
+
+panjang = int(input("masukan nilai panjang"))
+lebar = int(input("masukan nilai lebar"))
+luas_pp = panjang * lebar
+
+print("luas persegi panjang adalahaa: ", luas_pp)
+
+print("3.segitiga")
+
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi / 2
+print("luas segitiga adalah",luas_s)
