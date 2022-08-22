1c847d9f3aa10bc127533b4f4c4364734b3d0ac7
wildan <111479965+wildanbcil@users.noreply.github.com>
2022-08-21 22:32:39 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,16 @@
+n_=input("masukan nama depam: ")
+b_=input("masukan nama belakang: ")
+print("nama anda adalah",n_,b_)
+
+a=int(input("masukan nilai A: "))
+b=float(input("masukan nilai B: "))
+c=a+b
+
+print("A=", a)
+print("B=", b)
+print(a,"+",b,"=",c)
+
+x=(input("masukan nilai mata uang; "))
+y= x *13752
+print(x,"dollar=",y,"rupiah")
+ 
